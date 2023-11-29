ce4a22f0a1801bcf5d3084652e5097085a232bc3
alexvcs <alexcurtisslep@gmail.com>
2023-11-29 12:52:10 -0500

    setup design system, closes#2

---
+++ app/ui/fonts.ts
@@ -0,0 +1,3 @@
+import { Outfit } from "next/font/google";
+
+export const outfit = Outfit({subsets:["latin"], weight:["300", "500"]})
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,19 +2,26 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        "entertainment-red": "#FC4747",
+        "entertainment-black": "#10141E",
+        "entertainment-battleship-grey": "#5A698F",
+        "entertainment-dark-blue": "#161D2F",
+        "entertainment-white": "#FFFFFF"
       },
     },
   },
   plugins: [],
-}
+};
 export default config
