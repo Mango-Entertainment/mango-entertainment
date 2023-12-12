c81929e03232b5ea002aa25563dce78454aa3402
alexvcs <alexcurtisslep@gmail.com>
2023-12-12 13:15:04 -0500

    built trending card, bookmark icons
    Co-authored-by: Steve Smodish <stevesmodish@gmail.com>

--- app/ui/components/Trending.tsx
+++ app/ui/components/Trending.tsx
@@ -1,20 +1,18 @@
 import TrendingCard from "./TrendingCard";
 import {Tables} from "@/types"
 import { getTrending } from "@/app/lib/db";
-import { TrendingData } from "@/app/lib/definitions";
 
 const Trending = async () => {
-  // const trendingData = await getTrending();
-  const trendingData: TrendingData[] = await getTrending();
-  console.table(trendingData);
+  const trendingData = await getTrending();
+  // console.table(trendingData);
   if (!trendingData) return;
 
   return (
     <div className="ml-4 text-entertainment-pure-white">
       <h1 className="text-xl font-light">Trending</h1>
-      <div className="" id="carousel">
+      <div className="flex flex-nowrap gap-3 w-max" id="carousel">
         {trendingData.map((selection) => {
-          {console.log("selection", selection)}
+          // {console.log("selection", selection)}
           return <TrendingCard selection={selection} key={selection.id} />;
         })}
       </div>
--- app/ui/homepage/Homepage.tsx
+++ app/ui/homepage/Homepage.tsx
@@ -1,12 +1,10 @@
 import Search from "../components/Search";
 import Trending from "../components/Trending";
-import TrendingCard from "../components/TrendingCard";
 
 const Homepage = () => {
   return <div className="text-entertainment-greyish-blue">
     <Search />
     <Trending />
-    <TrendingCard />
     </div>;
 }
 
