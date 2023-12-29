a6a49ad550db8e625948077b796f83ff3c848245
alexvcs <alexcurtisslep@gmail.com>
2023-12-29 15:06:25 -0500

    working on zod and fun stuff
    Co-authored-by: Steve Smodish <stevesmodish@gmail.com>

--- app/ui/signup/Signup.tsx
+++ app/ui/signup/Signup.tsx
@@ -1,21 +1,59 @@
-"use client";
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import Link from 'next/link'
+import { headers, cookies } from 'next/headers'
+import { createClient } from '@/utils/supabase/server'
+import { redirect } from 'next/navigation'
+import Image from 'next/image'
 
-import Image from "next/image";
-import Link from "next/link";
-import {useRouter} from "next/navigation";
-import { useState, FormEvent, ChangeEvent } from "react";
+
+const FormFieldsSchema = z
+  .object({
+    email: z.string().email(),
+    password1: z
+      .string()
+      .min(7, { message: 'Must be at least 7 characters long' }),
+    password2: z.string(),
+  })
+  .refine((data) => data.password1 === data.password2, {
+    message: 'Passwords do not match',
+  })
+
+type FormFields = z.infer<typeof FormFieldsSchema>
 
 const Signup = () => {
-    const [email, setEmail] = useState<string>("");
-    const [passwordOne, setPasswordOne] = useState<string>("");
-    const [passwordTwo, setPasswordTwo] = useState<string>("");
-    const router = useRouter();
-    const [error, setError] = useState<string|null>(null);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormFields>({ resolver: zodResolver(FormFieldsSchema) })
 
+  // const signUpWithEmail = async ({email, password}) => {
+  //   'use server'
 
-    const onSubmit = (event: { preventDefault: () => void; }) => {
-      //fill in later
-    };
+  //   const origin = headers().get('origin')
+  //   // const email = formData.get('email') as string
+  //   // const password = formData.get('passwordOne') as string
+  //   // const password2 = formData.get('passwordTwo') as string
+  //   const cookieStore = cookies()
+  //   const supabase = createClient(cookieStore)
+
+  //   // if (password !== password2) return
+  //   const { error } = await supabase.auth.signUp({
+  //     email,
+  //     password,
+  //     options: {
+  //       emailRedirectTo: `${origin}/auth/callback`,
+  //     },
+  //   })
+
+  //   if (error) {
+  //     return redirect('/login?message=Could not authenticate user')
+  //   }
+
+  //   return redirect('/login?message=Check email to continue sign in process')
+  // }
 
   return (
     <div className="justify-center mt-12 grid justify-items-center md:mt-20">
@@ -31,33 +69,24 @@ const Signup = () => {
           <h1 className="mb-6 text-3xl font-light text-entertainment-pure-white">
             Sign Up
           </h1>
-          <form onSubmit={onSubmit}>
+          <form onSubmit={handleSubmit((d) => console.log("Field values: ", d, "Errors: ", errors ))}>
             <input
-              className="block w-full pb-4 pl-4 mb-3 text-sm font-light bg-transparent border-0 border-b-2 h-37 border-entertainment-greyish-blue text-entertainment-pure-white caret-entertainment-red focus:border-entertainment-pure-white"
-              type="email"
-              name="email"
-              value={email}
-              placeholder="Email address"
-              onChange={(event) => setEmail(event.target.value)}
-              required
+              {...register('email')}
+              // className="block w-full pb-4 pl-4 mb-3 text-sm font-light bg-transparent border-0 border-b-2 h-37 border-entertainment-greyish-blue text-entertainment-pure-white caret-entertainment-red focus:border-entertainment-pure-white"
+              // placeholder="Email address"
+              // required
             />
             <input
-              className="block w-full pb-4 pl-4 mb-3 text-sm font-light bg-transparent border-0 border-b-2 h-37 border-entertainment-greyish-blue text-entertainment-pure-white caret-entertainment-red focus:border-entertainment-pure-white"
-              type="password"
-              placeholder="Password"
-              name="passwordOne"
-              value={passwordOne}
-              onChange={(event) => setPasswordOne(event.target.value)}
-              required
+              {...register('password1')}
+              // className="block w-full pb-4 pl-4 mb-3 text-sm font-light bg-transparent border-0 border-b-2 h-37 border-entertainment-greyish-blue text-entertainment-pure-white caret-entertainment-red focus:border-entertainment-pure-white"
+              // placeholder="Password"
+              // required
             />
             <input
-              className="block w-full pb-4 pl-4 mb-10 text-sm font-light bg-transparent border-0 border-b-2 h-37 border-entertainment-greyish-blue text-entertainment-pure-white caret-entertainment-red focus:border-entertainment-pure-white"
-              type="password"
-              placeholder="Repeat Password"
-              name="passwordTwo"
-              onChange={(event) => setPasswordTwo(event.target.value)}
-              value={passwordTwo}
-              required
+              {...register('password2')}
+              // className="block w-full pb-4 pl-4 mb-10 text-sm font-light bg-transparent border-0 border-b-2 h-37 border-entertainment-greyish-blue text-entertainment-pure-white caret-entertainment-red focus:border-entertainment-pure-white"
+              // placeholder="Repeat Password"
+              // required
             />
             <button
               className="w-full h-12 mb-6 text-sm font-light text-entertainment-pure-white hover:text-entertainment-dark-blue hover:bg-entertainment-pure-white bg-entertainment-red rounded-md"
@@ -75,7 +104,7 @@ const Signup = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Signup;
+export default Signup
