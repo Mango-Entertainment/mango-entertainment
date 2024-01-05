77d938a44175d3507dcd24dfcd9bf440128f8013
alexvcs <alexcurtisslep@gmail.com>
2024-01-05 12:03:59 -0500

    working on redirect url
    Co-authored-by: Steve Smodish <stevesmodish@gmail.com>

--- app/ui/signup/Signup.tsx
+++ app/ui/signup/Signup.tsx
@@ -8,7 +8,18 @@ import { headers, cookies } from 'next/headers'
 import { createClient } from '@/utils/supabase/client'
 import { useRouter } from 'next/navigation'
 import Image from 'next/image'
-// import { signUpWithEmail } from '@/app/signup/route'
+
+const getURL = () => {
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    'http://localhost:3000/'
+  // Make sure to include `https://` when not localhost.
+  url = url.includes('http') ? url : `https://${url}`
+  // Make sure to include a trailing `/`.
+  url = url.charAt(url.length - 1) === '/' ? url : `${url}/`
+  return url
+}
 
 const FormFieldsSchema = z
   .object({
@@ -40,10 +51,10 @@ const Signup = () => {
       email,
       password,
       options: {
-        emailRedirectTo: `/login`,
+        emailRedirectTo: `${getURL()}login`,
       },
     })
-    if(res.data.user?.email) router.push("/login")
+    if(res.data.user?.email) router.push(`${getURL()}login`)
   }
 
   return (
