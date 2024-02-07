794373d778a0c6a9f5e11a73b87a874c5cffd799
alexvcs <alexcurtisslep@gmail.com>
2024-02-07 11:09:59 -0500

    hot fix fresh out of the oven
    Co-authored-by: Steve Smodish <stevesmodish@gmail.com>

--- utils/trpc-provider.tsx
+++ utils/trpc-provider.tsx
@@ -12,7 +12,7 @@ export const TrpcProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const url = process.env.NEXT_PUBLIC_SITE_URL
-    ? `https://${process.env.NEXT_PUBLIC_SITE_URL}/api/trpc/`
+    ? `${process.env.NEXT_PUBLIC_SITE_URL}api/trpc/`
     : 'http://localhost:3000/api/trpc/'
 
   const [trpcClient] = useState(() =>
