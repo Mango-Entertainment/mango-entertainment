7c3ee035a3fbd6e8fc2edfa38dcda20b5bcfda5e
alexvcs <alexcurtisslep@gmail.com>
2024-03-18 14:00:53 -0400

    only shows navbar buttons when logged in. closes #85
    Co-authored-by: Steve Smodish <stevesmodish@gmail.com>

--- app/_ui/components/Navbar.tsx
+++ app/_ui/components/Navbar.tsx
@@ -1,12 +1,16 @@
-import Image from "next/image";
-import Link from "next/link";
-import HomeIcon from "@/app/_ui/components/HomeIcon";
-import TvIcon from "@/app/_ui/components/TvIcon";
-import BookmarkIcon from "@/app/_ui/components/BookmarkIcon";
-import MovieIcon from "@/app/_ui/components/MovieIcon";
-import Auth from "@/app/_ui/components/Auth";
+"use client"
+
+import Image from 'next/image'
+import Link from 'next/link'
+import HomeIcon from '@/app/_ui/components/HomeIcon'
+import TvIcon from '@/app/_ui/components/TvIcon'
+import BookmarkIcon from '@/app/_ui/components/BookmarkIcon'
+import MovieIcon from '@/app/_ui/components/MovieIcon'
+import Auth from '@/app/_ui/components/Auth'
+import { useAuth } from '@clerk/nextjs'
 
 const Navbar = () => {
+  const { isSignedIn } = useAuth()
 
   return (
     <div className="grid grid-cols-3 lg:grid-cols-1 lg:h-screen lg:grid-rows-[2fr_3fr_9fr] lg:place-items-start bg-entertainment-semi-dark-blue items-center md:mx-6 md:w-auto md:mt-6 md:rounded-xl lg:max-h-[960px] sticky h-auto w-full lg:mx-0 lg:justify-self-center lg:w-24">
@@ -15,20 +19,24 @@ const Navbar = () => {
           <Image src="/logo.svg" alt="icon" width={32} height={25} />
         </Link>
       </div>
-      <div className="flex justify-between lg:flex-col lg:justify-self-center lg:h-full lg:w-5">
-        <Link href="/" className="w-4 md:w-5">
-          <HomeIcon />
-        </Link>
-        <Link href="/movies" className="w-4 md:w-5">
-          <MovieIcon />
-        </Link>
-        <Link href="/series" className="w-4 md:w-5">
-          <TvIcon />
-        </Link>
-        <Link href="/bookmarks" className="w-4 md:w-5">
-          <BookmarkIcon />
-        </Link>
-      </div>
+      {isSignedIn ? (
+        <div className="flex justify-between lg:flex-col lg:justify-self-center lg:h-full lg:w-5">
+          <Link href="/" className="w-4 md:w-5">
+            <HomeIcon />
+          </Link>
+          <Link href="/movies" className="w-4 md:w-5">
+            <MovieIcon />
+          </Link>
+          <Link href="/series" className="w-4 md:w-5">
+            <TvIcon />
+          </Link>
+          <Link href="/bookmarks" className="w-4 md:w-5">
+            <BookmarkIcon />
+          </Link>
+        </div>
+      ) : (
+        <div></div>
+      )}
       <div className="flex justify-end lg:justify-self-center lg:self-end">
         <div>
           <Auth />
@@ -36,6 +44,6 @@ const Navbar = () => {
       </div>
     </div>
   )
-};
+}
 
-export default Navbar;
+export default Navbar
