652b7440dd00b4c1203df058d9c6eef7fb86df8a
alexvcs <alexcurtisslep@gmail.com>
2024-03-28 19:55:55 +0000

    added mango logo and favicon
    Co-authored-by: Steve Smodish <stevesmodish@gmail.com>

--- app/_ui/components/Navbar.tsx
+++ app/_ui/components/Navbar.tsx
@@ -13,10 +13,10 @@ const Navbar = () => {
   const { isSignedIn } = useAuth()
 
   return (
-    <div className="grid grid-cols-3 lg:grid-cols-1 lg:h-screen lg:grid-rows-[2fr_3fr_9fr] lg:place-items-start bg-entertainment-semi-dark-blue items-center md:mx-6 md:w-auto md:mt-6 md:rounded-xl lg:max-h-[960px] sticky h-auto w-full lg:mx-0 lg:justify-self-center lg:w-24">
-      <div className="w-6 h-5 my-4 ml-4 lg:ml-0 md:ml-6 md:my-6 md:w-8 md:h-6 lg:mt-8 lg:justify-self-center">
-        <Link href="/" className="w-4 md:w-5">
-          <Image src="/logo.svg" alt="icon" width={32} height={25} />
+    <div className="grid grid-cols-3 h-20 lg:grid-cols-1 lg:h-screen lg:grid-rows-[3fr_3fr_9fr] lg:place-items-start bg-entertainment-semi-dark-blue items-center md:mx-6 md:w-auto md:mt-6 md:rounded-xl lg:max-h-[960px] sticky h-auto w-full lg:mx-0 lg:justify-self-center lg:w-20">
+      <div className="w-10 h-8 mt-5 mb-6 ml-4 lg:ml-0 md:ml-6 lg:mt-8 lg:justify-self-center">
+        <Link href="/" className="w-8 md:w-10">
+          <Image src="/mango-logo.png" alt="icon" width={64} height={50} />
         </Link>
       </div>
       {isSignedIn ? (
