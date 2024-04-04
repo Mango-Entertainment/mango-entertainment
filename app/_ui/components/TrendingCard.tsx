a8d5aea869c50da80a111420de02b15381a16819
alexvcs <alexcurtisslep@gmail.com>
2024-04-04 16:37:39 +0000

    refactored regular and trending cards to shadcn
    Co-authored-by: Steve Smodish <stevesmodish@gmail.com>

--- app/_ui/components/TrendingCard.tsx
+++ app/_ui/components/TrendingCard.tsx
@@ -2,6 +2,14 @@ import { type FC } from 'react'
 import Image from 'next/image'
 import { useUser } from '@clerk/nextjs'
 import useBookmarks from '@/app/_hooks/useBookmarks'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
 
 interface TrendingCardProps {
   id: string
@@ -29,40 +37,43 @@ const TrendingCard: FC<TrendingCardProps> = ({
   const toggleBookmark = useBookmarks()
 
   return (
-    <div className="relative entertainment-pure-white w-60 md:w-auto">
-      <Image
-        className="rounded-lg"
-        src={imageString}
-        width={470}
-        height={230}
-        alt="trending image"
-      />
-      {isSignedIn ? (
-        <div
-          onClick={() => toggleBookmark({ selection_id: id, user_id: user.id })}
-          className="absolute flex content-center justify-center top-2 right-2 md:top-4 md:right-6"
-        >
-          {bookmarked ? (
-            <Image
-              src="/icon-bookmark-full.svg"
-              height={32}
-              width={32}
-              alt="bookmark icon"
-            />
-          ) : (
-            <Image
-              src="/icon-bookmark-empty.svg"
-              height={32}
-              width={32}
-              alt="bookmark icon"
-            />
-          )}
-        </div>
-      ) : (
-        <></>
-      )}
-      <div className="absolute bottom-0 w-full p-3 rounded-b-lg md:p-6 bg-gradient-to-b from-transparent to-black/75">
-        <div className="flex items-center text-xs font-light opacity-75 gap-2 md:text-base">
+    <Card variant={'trending'}>
+      <CardContent>
+        <Image
+          className="rounded-lg"
+          src={imageString}
+          width={470}
+          height={230}
+          alt="trending image"
+        />
+        {isSignedIn ? (
+          <CardHeader
+            onClick={() =>
+              toggleBookmark({ selection_id: id, user_id: user.id })
+            }
+          >
+            {bookmarked ? (
+              <Image
+                src="/icon-bookmark-full.svg"
+                height={32}
+                width={32}
+                alt="bookmark icon"
+              />
+            ) : (
+              <Image
+                src="/icon-bookmark-empty.svg"
+                height={32}
+                width={32}
+                alt="bookmark icon"
+              />
+            )}
+          </CardHeader>
+        ) : (
+          <></>
+        )}
+      </CardContent>
+      <CardFooter className="absolute bottom-0 rounded-b-lg bg-gradient-to-b from-transparent to-black/75 p-3 md:p-6">
+        <CardDescription className="gap-2 text-xs md:text-base">
           {year}
           <span className="text-sm opacity-50 md:text-xl">•</span>
           <Image
@@ -75,10 +86,10 @@ const TrendingCard: FC<TrendingCardProps> = ({
           {category}
           <span className="text-sm opacity-50 md:text-xl">•</span>
           {rating}
-        </div>
-        <div className="text-sm font-medium md:text-2xl">{title}</div>
-      </div>
-    </div>
+        </CardDescription>
+        <CardTitle className="md:text-2xl">{title}</CardTitle>
+      </CardFooter>
+    </Card>
   )
 }
 
