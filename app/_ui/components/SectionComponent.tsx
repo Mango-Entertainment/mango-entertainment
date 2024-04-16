e45dc7dee6e3a9754f3f91803aed37bbba8c742d
alexvcs <alexcurtisslep@gmail.com>
2024-04-16 17:00:57 +0000

    added no results found for search
    Co-authored-by: Steve Smodish <stevesmodish@gmail.com>

--- app/_ui/components/SectionComponent.tsx
+++ app/_ui/components/SectionComponent.tsx
@@ -2,7 +2,6 @@ import RegularCard from '@/app/_ui/components/RegularCard'
 import { type FC } from 'react'
 import { type RouterOutputs } from '@/app/api/trpc/trpc-router'
 
-
 type SectionComponentProps = {
   sectionData: RouterOutputs['getRecommended'] | undefined
   section: string
@@ -14,14 +13,25 @@ const SectionComponent: FC<SectionComponentProps> = ({
   section,
   bookmarks,
 }) => {
-  if (sectionData && sectionData.results < 1) return <></>
+  if (sectionData && sectionData.results < 1) {
+    return (
+      <div className="ml-4 text-entertainment-pure-white">
+        <h1 className="mb-4 text-xl font-light md:mb-6 md:text-3xl lg:mb-8">
+          {section}
+        </h1>
+        <p className="text-center font-light opacity-75 lg:text-xl">
+          No results found
+        </p>
+      </div>
+    )
+  }
 
   return (
     <div className="ml-4 text-entertainment-pure-white">
-      <h1 className="mb-4 text-xl font-light md:text-3xl md:mb-6 lg:mb-8">
+      <h1 className="mb-4 text-xl font-light md:mb-6 md:text-3xl lg:mb-8">
         {section}
       </h1>
-      <div className="grid grid-cols-[repeat(auto-fill,minmax(140px,1fr))] md:grid-cols-[repeat(auto-fill,minmax(220px,1fr))] lg:grid-cols-[repeat(auto-fill,minmax(280px,1fr))] gap-4 mb-8">
+      <div className="mb-8 grid grid-cols-[repeat(auto-fill,minmax(140px,1fr))] gap-4 md:grid-cols-[repeat(auto-fill,minmax(220px,1fr))] lg:grid-cols-[repeat(auto-fill,minmax(280px,1fr))]">
         {sectionData?.data.map((selection) => {
           const bookmarked = bookmarks?.data.filter(
             (bookmark) => bookmark.selection_id === selection.id,
