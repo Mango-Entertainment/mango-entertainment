8c0fb41ec711d5727363940ce41ba55d7206232b
alexvcs <alexcurtisslep@gmail.com>
2024-05-03 19:27:49 +0000

    deleting unused files and folders
    Co-authored-by: Steve Smodish <stevesmodish@gmail.com>

--- app/_ui/bookmarkpage/Bookmarks.tsx
+++ app/_ui/bookmarkpage/Bookmarks.tsx
@@ -2,33 +2,17 @@
 
 import { trpc } from '@/lib/server/trpc'
 import Search from '@/app/_ui/components/Search'
-import SectionComponent from '@/app/_ui/components/Deprecated/SectionComponent'
-import { type ChangeEvent, type FC, useState } from 'react'
+import { type ChangeEvent, useState } from 'react'
 import { useUser } from '@clerk/nextjs'
 import SkeletonSectionComponent from '@/app/_ui/components/SkeletonSectionComponent'
-import { type TmdbMovieDetailsData } from '@/lib/server/routes/tmdb/tmdb'
-import { useQuery } from '@tanstack/react-query'
-import BookmarkMovieCard from '../components/Movies/BookmarkMovieCard'
-import useBookmarks from '@/app/_hooks/useBookmarks'
-import BookmarkSeriesCard from '../components/Series/BookmarkSeriesCard'
 import MovieSectionComponent from '../components/Movies/MovieSectionComponent'
 import SeriesSectionComponent from '../components/Series/SeriesSectionComponent'
-
-interface BookmarkSection {
-  search: string
-  user_id: string
-}
 
 const Bookmarks = () => {
   const [search, setSearch] = useState('')
 
   const { user } = useUser()
   const user_id = user?.id ?? ''
-
-  const bookmarks = trpc.bookmarks.getBookmarks.useQuery({
-    search: search,
-    user_id: user?.id ?? '',
-  })
 
   const movieBookmarks = trpc.bookmarks.getBookmarks.useQuery({
     search: search,
@@ -50,14 +34,6 @@ const Bookmarks = () => {
     search,
     user_id,
   })
-
-  // const bookmarkedSeriesData = bookmarkedSeries.data?.map(
-  //   (item) => item,
-  // )
-
-  // const bookmarkedMovieData = bookmarkedMovies.data?.map(
-  //   (item) => item,
-  // )
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value)
@@ -84,62 +60,6 @@ const Bookmarks = () => {
         />
       )}
     </div>
-  )
-  // return (
-  //   <>
-  //     <Search search={search} handleChange={handleChange} />
-  //     <BookmarkedMovies user_id={user_id} search={search} />
-  //     <BookmarkedSeries user_id={user_id} search={search} />
-  //   </>
-  // )
-}
-
-const BookmarkedMovies: FC<BookmarkSection> = ({ user_id, search }) => {
-  const { data, isLoading } = trpc.tmdb.getBookmarkedMovies.useQuery({
-    search: search,
-    user_id: user_id,
-  })
-
-  if (isLoading) return <>loading</>
-  return (
-    <>
-      <h2 className="text-entertainment-pure-white">Movie Bookmarks</h2>
-      <ul className="flex flex-wrap gap-4 text-entertainment-pure-white">
-        {data?.map((selection) => {
-          return (
-            <BookmarkMovieCard
-              key={selection.selection_id}
-              id={selection.selection_id}
-              search={search}
-            />
-          )
-        })}
-      </ul>
-    </>
-  )
-}
-
-const BookmarkedSeries: FC<BookmarkSection> = ({ user_id, search }) => {
-  const { data, isLoading } = trpc.tmdb.getBookmarkedSeries.useQuery({
-    search: search,
-    user_id: user_id,
-  })
-  if (isLoading) return <>loading</>
-  return (
-    <>
-      <h2 className="text-entertainment-pure-white">Series Bookmarks</h2>
-      <ul className="text-entertainment-pure-white">
-        {data?.map((selection) => {
-          return (
-            <BookmarkSeriesCard
-              key={selection.selection_id}
-              id={selection.selection_id}
-              search={search}
-            />
-          )
-        })}
-      </ul>
-    </>
   )
 }
 
