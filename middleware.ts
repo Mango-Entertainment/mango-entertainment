7fc2f1bcd9b267ac7f17607797ef5571fd9b44a9
alexvcs <alexcurtisslep@gmail.com>
2024-05-07 18:10:32 +0000

    trying to get the deploy working with middleware
    Co-authored-by: Steve Smodish <stevesmodish@gmail.com>

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,42 @@
 import { authMiddleware } from '@clerk/nextjs'
+import { type NextRequest, NextResponse } from 'next/server'
+ 
+const allowedOrigins = ['http://localhost:3000', 'https://mango-entertainment.vercel.app/']
+ 
+const corsOptions = {
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+}
+ 
+export function middleware(request: NextRequest) {
+  // Check the origin from the request
+  const origin = request.headers.get('origin') ?? ''
+  const isAllowedOrigin = allowedOrigins.includes(origin)
+ 
+  // Handle preflighted requests
+  const isPreflight = request.method === 'OPTIONS'
+ 
+  if (isPreflight) {
+    const preflightHeaders = {
+      ...(isAllowedOrigin && { 'Access-Control-Allow-Origin': origin }),
+      ...corsOptions,
+    }
+    return NextResponse.json({}, { headers: preflightHeaders })
+  }
+ 
+  // Handle simple requests
+  const response = NextResponse.next()
+ 
+  if (isAllowedOrigin) {
+    response.headers.set('Access-Control-Allow-Origin', origin)
+  }
+ 
+  Object.entries(corsOptions).forEach(([key, value]) => {
+    response.headers.set(key, value)
+  })
+ 
+  return response
+}
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
