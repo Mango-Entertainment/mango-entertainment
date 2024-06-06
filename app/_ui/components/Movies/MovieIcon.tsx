5e093f8845d9405fcb959a6eaadd87a2417a6a39
alexvcs <alexcurtisslep@gmail.com>
2024-06-06 17:48:28 +0000

    changed navigation icon styles
    Co-authored-by: Steve Smodish <stevesmodish@gmail.com>

--- app/_ui/components/Movies/MovieIcon.tsx
+++ app/_ui/components/Movies/MovieIcon.tsx
@@ -1,12 +1,19 @@
-const MovieIcon = () => {
+import { cx } from 'class-variance-authority'
+
+const MovieIcon = ({ pathName }: { pathName: string }) => {
   return (
     <svg
       viewBox="0 0 20 20"
-      className="fill-entertainment-greyish-blue hover:fill-entertainment-pure-white"
+      className={cx(
+        'hover:scale-125',
+        pathName === '/movies'
+          ? 'fill-entertainment-pure-white'
+          : 'fill-entertainment-greyish-blue',
+      )}
     >
       <path d="M16.956 0H3.044A3.044 3.044 0 0 0 0 3.044v13.912A3.044 3.044 0 0 0 3.044 20h13.912A3.044 3.044 0 0 0 20 16.956V3.044A3.044 3.044 0 0 0 16.956 0ZM4 9H2V7h2v2Zm-2 2h2v2H2v-2Zm16-2h-2V7h2v2Zm-2 2h2v2h-2v-2Zm2-8.26V4h-2V2h1.26a.74.74 0 0 1 .74.74ZM2.74 2H4v2H2V2.74A.74.74 0 0 1 2.74 2ZM2 17.26V16h2v2H2.74a.74.74 0 0 1-.74-.74Zm16 0a.74.74 0 0 1-.74.74H16v-2h2v1.26Z" />
     </svg>
-  );
+  )
 }
 
 export default MovieIcon
