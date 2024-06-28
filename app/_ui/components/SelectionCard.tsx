dc576fda438b8423d250af8fc11041a29c7aa688
alexvcs <alexcurtisslep@gmail.com>
2024-06-28 17:33:30 +0000

    finished styling selectionCard
    Co-authored-by: Steve Smodish <stevesmodish@gmail.com>

--- app/_ui/components/SelectionCard.tsx
+++ app/_ui/components/SelectionCard.tsx
@@ -43,67 +43,73 @@ const SelectionCard: FC<SelectionCardProps> = ({
         }
       >
         <Card variant={'selection'}>
+          {user.data ? (
+            <CardHeader
+              onClick={async (event: MouseEvent<HTMLDivElement>) => {
+                event.preventDefault()
+                event.stopPropagation()
+                await toggleBookmark({
+                  selection_id: selection_id,
+                  user_id: user?.data?.id ?? '',
+                  selection_type: selection_type,
+                  selection_title: selection_title,
+                  selection_poster_path: selection_poster_path ?? '',
+                  selection_year: selection_year,
+                })
+              }}
+              className="click:scale-100 h-12 w-12 absolute top-0 right-0 z-10 origin-center cursor-pointer p-2 transition-transform duration-500 hover:scale-125 md:p-3 md:hover:scale-150"
+            >
+              {bookmarked ? (
+                <Image
+                  src="/icon-bookmark-full.svg"
+                  height={32}
+                  width={32}
+                  alt="bookmark icon"
+                />
+              ) : (
+                <Image
+                  src="/icon-bookmark-empty.svg"
+                  height={32}
+                  width={32}
+                  alt="bookmark icon"
+                />
+              )}
+            </CardHeader>
+          ) : (
+            <></>
+          )}
           <AspectRatio ratio={2 / 3}>
-            <picture>
-              <source
-                media="(max-width: 767px)"
-                srcSet={`https://image.tmdb.org/t/p/w185${selection_poster_path}`}
-              />
-              <source
-                media="(min-width: 768px)"
-                srcSet={`https://image.tmdb.org/t/p/w342${selection_poster_path}`}
-              />
-              <img
-                src={`https://image.tmdb.org/t/p/w342${selection_poster_path}}`}
-                alt={`${selection_title} poster`}
-                className="w-full rounded-lg"
-              />
-            </picture>
+            {selection_poster_path ? (
+              <picture>
+                <source
+                  media="(max-width: 767px)"
+                  srcSet={`https://image.tmdb.org/t/p/w185${selection_poster_path}`}
+                />
+                <source
+                  media="(min-width: 768px)"
+                  srcSet={`https://image.tmdb.org/t/p/w342${selection_poster_path}`}
+                />
+                <img
+                  src={`https://image.tmdb.org/t/p/w342${selection_poster_path}}`}
+                  alt={`${selection_title} poster`}
+                  className="w-full rounded-lg"
+                />
+              </picture>
+            ) : (
+              <div className=" h-full w-full grow content-center bg-entertainment-semi-dark-blue">
+                <div className=" bg-entertainment-pure-white p-2 text-center text-xl text-entertainment-greyish-blue">
+                  No image available
+                </div>
+              </div>
+            )}
           </AspectRatio>
-          <CardFooter className="w-40 pt-2 md:w-56 flex space-between">
-            <div className='grow'>
+          <CardFooter className="w-40 pt-2 md:w-56">
               <CardTitle className="text-wrap md:text-lg">
                 {selection_title}
               </CardTitle>
               <CardDescription className="text-xs md:text-sm">
                 {selection_year}
               </CardDescription>
-            </div>
-            {user.data ? (
-              <CardHeader
-                onClick={async (event: MouseEvent<HTMLDivElement>) => {
-                  event.preventDefault()
-                  event.stopPropagation() 
-                  await toggleBookmark({
-                    selection_id: selection_id,
-                    user_id: user?.data?.id ?? '',
-                    selection_type: selection_type,
-                    selection_title: selection_title,
-                    selection_poster_path: selection_poster_path,
-                    selection_year: selection_year,
-                  })
-                }}
-                className="click:scale-100 origin-center cursor-pointer p-2 transition-transform duration-500 hover:scale-125 md:p-3 md:hover:scale-150"
-              >
-                {bookmarked ? (
-                  <Image
-                    src="/icon-bookmark-full.svg"
-                    height={32}
-                    width={32}
-                    alt="bookmark icon"
-                  />
-                ) : (
-                  <Image
-                    src="/icon-bookmark-empty.svg"
-                    height={32}
-                    width={32}
-                    alt="bookmark icon"
-                  />
-                )}
-              </CardHeader>
-            ) : (
-              <></>
-            )}
           </CardFooter>
         </Card>
       </Link>
