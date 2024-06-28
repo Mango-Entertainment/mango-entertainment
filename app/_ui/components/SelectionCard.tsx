ca34af5be62e23d379870edf89b8ada81e8699fc
alexvcs <alexcurtisslep@gmail.com>
2024-06-28 16:28:07 +0000

    added picture and source elements
    Co-authored-by: Steve Smodish <stevesmodish@gmail.com>

--- app/_ui/components/SelectionCard.tsx
+++ app/_ui/components/SelectionCard.tsx
@@ -42,77 +42,40 @@ const SelectionCard: FC<SelectionCardProps> = ({
 
   return (
     <div>
-      <Card variant={'selection'}>
-        <AspectRatio
-          style={{
-            backgroundImage: `url(https://image.tmdb.org/t/p/w500${selection_poster_path}})`,
+      {user.data ? (
+        <CardHeader
+          onClick={async (event) => {
+            event.stopPropagation()
+            await toggleBookmark({
+              selection_id: selection_id,
+              user_id: user?.data?.id ?? '',
+              selection_type: selection_type,
+              selection_title: selection_title,
+              selection_poster_path: selection_poster_path,
+              selection_year: selection_year,
+            })
           }}
-          className="rounded-lg bg-cover"
-          ratio={2 / 3}
+          className="click:scale-100 origin-center cursor-pointer p-2 transition-transform duration-500 hover:scale-125 md:p-3 md:hover:scale-150"
         >
-          <CardContent className="flex h-full flex-col justify-between">
-            <div className="flex flex-row justify-between">
-              <Link
-                href={
-                  selection_type === 'Movie'
-                    ? `movies/${selection_id}`
-                    : `series/${selection_id}`
-                }
-                className="h-full w-full grow"
-              />
-              {user.data ? (
-                <CardHeader
-                  onClick={() =>
-                    toggleBookmark({
-                      selection_id: selection_id,
-                      user_id: user?.data?.id ?? '',
-                      selection_type: selection_type,
-                      selection_title: selection_title,
-                      selection_poster_path: selection_poster_path,
-                      selection_year: selection_year,
-                    })
-                  }
-                  className="click:scale-100 origin-center cursor-pointer p-2 transition-transform duration-500 hover:scale-125 md:p-3 md:hover:scale-150"
-                >
-                  {bookmarked ? (
-                    <Image
-                      src="/icon-bookmark-full.svg"
-                      height={32}
-                      width={32}
-                      alt="bookmark icon"
-                    />
-                  ) : (
-                    <Image
-                      src="/icon-bookmark-empty.svg"
-                      height={32}
-                      width={32}
-                      alt="bookmark icon"
-                    />
-                  )}
-                </CardHeader>
-              ) : (
-                <></>
-              )}
-            </div>
-            <Link
-              href={
-                selection_type === 'Movie'
-                  ? `movies/${selection_id}`
-                  : `series/${selection_id}`
-              }
-              className="h-full w-full grow content-center"
-            >
-              {!selection_poster_path ? (
-                <div className="mb-12 bg-entertainment-pure-white p-2 text-center	text-xl text-entertainment-greyish-blue">
-                  No image available
-                </div>
-              ) : (
-                <></>
-              )}
-            </Link>
-          </CardContent>
-        </AspectRatio>
-      </Card>
+          {bookmarked ? (
+            <Image
+              src="/icon-bookmark-full.svg"
+              height={32}
+              width={32}
+              alt="bookmark icon"
+            />
+          ) : (
+            <Image
+              src="/icon-bookmark-empty.svg"
+              height={32}
+              width={32}
+              alt="bookmark icon"
+            />
+          )}
+        </CardHeader>
+      ) : (
+        <></>
+      )}
       <Link
         href={
           selection_type === 'Movie'
@@ -120,6 +83,59 @@ const SelectionCard: FC<SelectionCardProps> = ({
             : `series/${selection_id}`
         }
       >
+        <Card variant={'selection'}>
+          <AspectRatio
+            // style={{
+            //   backgroundImage: `url(https://image.tmdb.org/t/p/w500${selection_poster_path}})`,
+            // }}
+            ratio={2 / 3}
+          >
+            <picture>
+              <source
+                media="(max-width: 767px)"
+                srcSet={`https://image.tmdb.org/t/p/w185${selection_poster_path}`}
+              />
+              <source
+                media="(min-width: 768px)"
+                srcSet={`https://image.tmdb.org/t/p/w342${selection_poster_path}`}
+              />
+              <img
+                src={`https://image.tmdb.org/t/p/w342${selection_poster_path}}`}
+                alt={`${selection_title} poster`}
+                className="w-full rounded-lg"
+              />
+            </picture>
+            {/* <CardContent className="flex h-full flex-col justify-between">
+              <div className="flex flex-row justify-between">
+                <Link
+                  href={
+                    selection_type === 'Movie'
+                      ? `movies/${selection_id}`
+                      : `series/${selection_id}`
+                  }
+                  className="h-full w-full grow"
+                />
+              </div>
+              <Link
+                href={
+                  selection_type === 'Movie'
+                    ? `movies/${selection_id}`
+                    : `series/${selection_id}`
+                }
+                className="h-full w-full grow content-center"
+              >
+              {!selection_poster_path ? (
+                <div className="mb-12 bg-entertainment-pure-white p-2 text-center	text-xl text-entertainment-greyish-blue">
+                  No image available
+                </div>
+              ) : (
+                <></>
+              )}
+              </Link>
+            </CardContent> */}
+          </AspectRatio>
+        </Card>
+
         <CardFooter className="w-40 pt-2 md:w-56">
           <CardTitle className="text-wrap md:text-lg">
             {selection_title}
